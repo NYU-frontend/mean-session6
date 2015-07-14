165251fa5415e09e09f6b61b3a59d79a3e893010
DannyBoyNYC <daniel.deverell@gmail.com>
2015-07-14 09:42:58 -0400

    added validation to pirate portfolio

--- other/Angular/js/app.js
+++ other/Angular/js/app.js
@@ -1,19 +1,5 @@
-var portfolioApp = angular.module('portfolioApp', ["ngRoute"]);
-
-portfolioApp.config(function ($routeProvider) {
-
-$routeProvider
-	.when("/portfolios",  { controller: "PortfolioListController", templateUrl: "partials/articles.html" })
-	.when("/",  { redirectTo: "/portfolios" })
-  	.otherwise({ redirectTo: "/404_page" });
-
-});
-
-
+var portfolioApp = angular.module('portfolioApp', []);
 portfolioApp.controller('PortfolioListController', function( $scope ) {
-
-	$scope.new_portfolio = {};
-	$scope.add_portfolio_error = "";
 
 	$scope.portfolios = [
 	{name: 'vessel1309', title: 'Call of Booty', date: '2013-09-01', description: 'Arrrgh mateys! This be the finest vessel sailing these seas that yer eyes ever laid sight on. Arrrgh.', imageurl: 'drunkenPirate_thumb3.png' },
@@ -22,22 +8,41 @@ portfolioApp.controller('PortfolioListController', function( $scope ) {
 	]
 
 	$scope.addPortfolio = function (new_portfolio) {
-
-
-		if (!new_portfolio.title) {
-			$scope.add_portfolio_error = "Missing title"
-		}
-		else if (!new_portfolio.date || new_portfolio.date.length < 10){
-			$scope.add_portfolio_error = "You must provide a date in format yyyy/mm/dd";
-		} else if (!new_portfolio.description){
-			$scope.add_portfolio_error = "Missing description";
-		} else if (!new_portfolio.name) {
-			$scope.add_portfolio_error = "Missing name - six characters";
+		$scope.add_portfolio_error = "";
+		if (!new_portfolio.date || new_portfolio.date.length <10 ){
+			$scope.add_portfolio_error = "Please provide a date in yyyy/mm/dd format.";
+		} else if (!new_portfolio.title) {
+			$scope.add_portfolio_error = "Please provide a title.";
 		} else {
 			$scope.portfolios.push(new_portfolio);
 			$scope.adding_portfolio = {};
-			$scope.add_portfolio_error = "";
-		};
+		}
 	};
-
+	
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
